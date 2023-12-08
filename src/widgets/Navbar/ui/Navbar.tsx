e66bbd2aa40e210c8d8b6a887512907ba3c5c05e
mintolime <evgeniabright@yandex.ru>
2023-12-08 16:08:37 +0700

    refactor: translated Navlink inner

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,18 @@
 import { classNames } from 'shared/lib/helpers/classNames/classNames';
 import cls from './Navbar.module.scss';
 import { Applink } from 'shared/ui/AppLink/Applink';
+import { useTranslation } from 'react-i18next';
 interface NavBarProps {
   className?: string;
 }
 export const Navbar = ({ className }: NavBarProps) => {
+  const { t } = useTranslation();
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
     
       <div className={cls.links}>
-        <Applink to={'/'}>Главная</Applink>
-        <Applink to={'/news'}>Новости</Applink>
+        <Applink to={'/'}>{t('Главная')}</Applink>
+        <Applink to={'/news'}>{t('Новости')}</Applink>
       </div>
     </div>
   );
